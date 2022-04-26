098bd65cf32a2203524af88f6e1df89260ef5117
golemiam <52841560+golemiam@users.noreply.github.com>
2022-04-25 23:14:50 -0600

    Update index.py
    Added finished product (probably, turned it in, may make edits later.)

--- index.py
+++ index.py
@@ -12,193 +12,133 @@ of the 3 different people (Pa, Mi-Ma (shown as ma via dictionaries), and Reg.
 '''
 import subprocess
 import tempfile
-import turtle #Doing both turtle imports as you did both, don't want to miss something, 
+import turtle #Doing both turtle imports as you did both, don't want to miss something
 import random
-import turtle
 import statistics
 import sys
-from functools import reduce
+import functools
 def main():
     """
     Defines the main function. All of the code is contained within.
+    Use command line indices for function: position 1 is for
     """
 
+    pa_ = None
+    ma_ = None
+    reg = None
 
-    
-    screen = turtle.getscreen()
-    screen.setup(width = 300, height = 400)
-    turtle.shapesize(0.5, 0.5)
-    current = [0,0]
-    n = [0,1]
-    s = [0,-1]
-    e = [1,0]
-    w = [-1,0]
-    """
-    lengths = list(map(int(sys.argv[1].split(','))))
-    trials = int(sys,argv[2])
-    walker = sys.argv[3]
-    """
-    lengths = [100, 1000]
-    trials = 50
-    walker = 'all'
+
+    if sys.argv[3] == "Pa":
+        person = [pa_]
+    elif sys.argv[3] == "Mi-Ma":
+        person = [ma_]
+    elif sys.argv[3] == "Reg":
+        person = [reg]
+    elif sys.argv[3] == "all":
+        person = [pa_, ma_, reg]
 
 
 
 
-    """
-    a = end_point[0]
-    print(a)
-    """
-    """
-    c = 
-    distance = (end_point[0])**2
-    
-    max_distance = max(distances)
-    min_distance = min(distances)
-    average_distance = statistics.mean(distances)
-    std_dev = statistics.stdev(distances)
-    cv = std_dev / average
-    """
-    simulate([100, 1000], 50, "all")
+    simulate(sys.argv[1], sys.argv[2], {sys.argv[3]})
     plot()
     save_to_image(dest='random_walk.png')
-    #help(statistics.mean)
+
 def simulate(walk_lengths, trials, person):
     """
     Defines the simulate function that calculates the data and prints it to the screen.
     """
-
-    try:
-        condition = (sys.argv[1]).lower()
-        if condition == "100":
-            steps = 100
-        elif condition == "1000":
-            steps = 1000
-        elif condition == "10":
-            steps = 10
-    except IndexError:
-        print("Usage index 1: 100 for 100 steps, 1000 for 1000 steps.")
-        steps = 100
-    trials = 50
-
-
-    pa = {"name":"Pa", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[0,-1],[1,0],[-1,0]), "shape":'circle', "color":'black'}
-    ma = {"name":"Ma", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[1,0],[0,-1],[0,-1],[-1,0]), "shape":'square', "color":'green'}
+    steps = int(str(walk_lengths).strip("[]"))
+    pa_ = {"name":"Pa", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[0,-1],[1,0],[-1,0]), "shape":'circle', "color":'black'}
+    ma_ = {"name":"Mi-Ma", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[1,0],[0,-1],[0,-1],[-1,0]), "shape":'square', "color":'green'}
     reg = {"name":"Reg", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([1,0],[-1,0]), "shape":'triangle', "color":'red'}
-    people = [pa, ma, reg]
-    run_trials(people, trials, steps)
-    for person in people:
-        for _i in range(trials):
-            person['cur'] = list(person['start'])
-            for _j in range(steps):
-                choice = random.choice(person['rules'])
-                person['cur'] = step(person['cur'], choice)
-                person["end_point"].append(person["cur"])
-                #end of j in steps
-                
-                
-                
-                
-                
-                
-                
-                lengths_x = []
-                lengths_y = []
-                list_dist = []
-                average_list = []
+    if person == {'Pa'}:
+        person = pa_
+    elif person == {'Mi-Ma'}:
+        person = ma_
+    elif person == {'Reg'}:
+        person = reg
+    elif person == {'all'}:
+        people = [pa_, ma_, reg]
+        for person in people:
+            for _i in range(int(trials)):
+                person['cur'] = list(person['start'])
+                for _j in range(steps):
+                    choice = random.choice(person['rules'])
+                    person['cur'] = step(person['cur'], choice)
+        person["end_point"].append(person["cur"])
+    else:
+        print(person)
+    for _i in range(int(trials)):
+        person['cur'] = list(person['start'])
+        for _j in range(steps):
+            choice = random.choice(person['rules'])
+            person['cur'] = step(person['cur'], choice)
+    person["end_point"].append(person["cur"])
 
-                for point in person['end_point']:
-                    x_end_point = point[0] * 5
-                    y_end_point = point[1] * 5
-                    lengths_x.append((x_end_point)**2)
-                    lengths_y.append((y_end_point)**2)
+    lengths_x = []
+    lengths_y = []
+    list_dist = []
+
+    for point in person['end_point']:
+        x_end_point = point[0] * 5
+        y_end_point = point[1] * 5
+        lengths_x.append((x_end_point)**2)
+        lengths_y.append((y_end_point)**2)
         sum_xy = lengths_x + lengths_y
-    def average_distance():
-        
-        summ = reduce(lambda x, y: x + y, sum_xy)
+        summ = functools.reduce(lambda x, y: x + y, sum_xy)
         dist = summ**0.5
         list_dist.append(dist)
-        length = len(sum_xy)
-        average = dist/(len(sum_xy))
-        
-        print(lengths_y)
-        print(lengths_x)
-        print(sum_xy)
-        print(summ)
-        print(dist)   
-        print(list_dist)
-        print(length)
-    
-        print(average)
-        print(f"{person['name']} random walk of 100 steps")
-        print(f"{person['name']} Mean {average:.1f}")
-        
-    average_distance()
-    def standard_deviation():
-        pass
-        
-
-    
-    
-
-    
-    
-    #print(f"Pa Mean: {average[0]}")
-
-            
+        average = statistics.mean(sum_xy)
+        standard_deviation = statistics.stdev(sum_xy)
+        coefficient = standard_deviation/average
+        maximum = max(sum_xy)
+        minimum = min(sum_xy)
+        print(f"{person['name']} random walk of {steps} steps")
+        print(f"Mean = {average:.1f} CV = {coefficient:.1f} ")
+        print(f"Max = {maximum:.1f} Min = {minimum:.1f} ")
 
 def plot():
     """
-    Function that draws the endpoints of the calculations for the 50 trials of 100 steps. 
+    Function that draws the endpoints of the calculations for the 50 trials of 100 steps.
     """
+    screen = turtle.getscreen()
+    screen.setup(width = 300, height = 400)
+    turtle.shapesize(0.5, 0.5)
     turtle.tracer(n = 99, delay = 50)
     steps = 100
     trials = 50
 
 
-    pa = {"name":"Pa", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[0,-1],[1,0],[-1,0]), "shape":'circle', "color":'black'}
-    ma = {"name":"Mi-Ma", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[0,-1],[1,0],[-1,0],[0,-1]), "shape":'square', "color":'green'}
+    pa_ = {"name":"Pa", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[0,-1],[1,0],[-1,0]), "shape":'circle', "color":'black'}
+    ma_ = {"name":"Mi-Ma", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([0,1],[1,0],[0,-1],[0,-1],[-1,0]), "shape":'square', "color":'green'}
     reg = {"name":"Reg", "start":(0,0), "cur":[0,0], "end_point":[], "distance":[], "rules":([1,0],[-1,0]), "shape":'triangle', "color":'red'}
-    people = [pa, ma, reg]
+    people = [pa_, ma_, reg]
 
     for person in people:
         for _i in range(trials):
             person['cur'] = list(person['start'])
             for _j in range(steps):
-                choice = random.choice(person['rules'])
-                person['cur'] = step(person['cur'], choice)
+                choose = random.choice(person['rules'])
+                person['cur'] = step(person['cur'], choose)
                 person["end_point"].append(person["cur"])
                 #end of j in steps
-                
+
                 color = person['color']
                 shape = person['shape']
-                
+
                 turtle.up()
                 turtle.color(color)
                 turtle.shape(shape)
                 for point in person['end_point']:
                     x_end_point = point[0] * 5
                     y_end_point = point[1] * 5
-                    
-            
+
+
             turtle.goto(x_end_point, y_end_point)
             turtle.stamp()
 
 
-
-def run_trials(people, trials, steps):
-    """
-    defines function that runs the individual trials and prints to the screen.
-    """
-    for person in people:
-        for _i in range(trials):
-            person['cur'] = list(person['start'])
-            for _j in range(steps):
-                choice = random.choice(person['rules'])
-                person['cur'] = step(person['cur'], choice)
-                #end of j in steps
-            print(f"{person['name']}: End of trial number {_i} location: {person['cur']}")
-    
 def step(current, direction):
     """
     defines how to move each step.
@@ -221,11 +161,10 @@ def save_to_image(dest='random_walk.png'):
                         '-sDEVICE=png16m',
                         tmp.name],
                        stdout=subprocess.DEVNULL)
-    
+
 if __name__== "__main__":
-    """
-    Program starts here.
-    """
     main()
+
+
 
 
